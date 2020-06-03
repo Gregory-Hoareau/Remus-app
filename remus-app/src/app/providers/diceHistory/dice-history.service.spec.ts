39e803221ccf087c82963de575b316f188c0e181
AnthonyChoquard <anthony.choquard@etu.univ-cotedazur.fr>
2020-06-03 18:13:44 +0200

    diceHistoryService test #17
    test des méthode addDiceRoll et selectedDiceString du siceHistoryService

--- remus-app/src/app/providers/diceHistory/dice-history.service.spec.ts
+++ remus-app/src/app/providers/diceHistory/dice-history.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DiceHistoryService } from './dice-history.service';
+import {Dice} from "../../models/dice.model";
+import {DiceRoll} from "../../models/dice-roll.model";
 
 describe('DiceHistoryService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +10,25 @@ describe('DiceHistoryService', () => {
   it('should be created', () => {
     const service: DiceHistoryService = TestBed.get(DiceHistoryService);
     expect(service).toBeTruthy();
+  });
+
+  it('should add a roll to history', () => {
+    const service: DiceHistoryService = TestBed.get(DiceHistoryService);
+    const d4: Dice =  {name: 'd4', value: 4};
+    const d12: Dice =  {name: 'd12', value: 12};
+    const diceSelected = new Map<Dice, number>();
+    diceSelected.set(d4, 3);
+    diceSelected.set(d12, 2);
+    const roll = {
+      dices: diceSelected,
+      modificator: 5,
+      result: 29,
+      separatedValue: '3 - 2 - 3 - 9 - 12'
+    } as DiceRoll;
+    service.addDiceRoll(roll);
+    service.addDiceRoll(roll);
+    expect(service.history.length).toBe(2);
+    expect(service.history[0]).toEqual(roll);
+    expect(service.selectedDiceString(roll)).toEqual('3d4 + 2d12 + 5');
   });
 });
