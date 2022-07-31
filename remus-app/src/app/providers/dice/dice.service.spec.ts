58e28329fadf7f777f27eb988b285269b0eed246
Graulier Brice <brice.graulier@gmail.com>
2022-07-31 14:02:05 +0200

    Test part 2

--- remus-app/src/app/providers/dice/dice.service.spec.ts
+++ remus-app/src/app/providers/dice/dice.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { DiceRoll } from 'src/app/models/dice-roll.model';
 import { Dice } from 'src/app/models/dice.model';
 
 import { DiceService } from './dice.service';
@@ -10,23 +11,24 @@ describe('DiceService', () => {
     const service: DiceService = TestBed.get(DiceService);
     expect(service).toBeTruthy();
   });
+
   it('should add and roll dice', () => {
     const service: DiceService = TestBed.get(DiceService);
     service.AddSelectedDice(new Dice('d4', 4));
     service.AddSelectedDice(new Dice('d6', 6));
-    service.launchDice();
-    
-    expect(component.result).toBeGreaterThanOrEqual(2);
-    expect(component.result).toBeLessThanOrEqual(10);
-    expect(component.totalDiceSum).toBe(10);
+    const resultRoll:DiceRoll = service.launchDice();
+
+    expect(resultRoll.result).toBeGreaterThanOrEqual(2);
+    expect(resultRoll.result).toBeLessThanOrEqual(10);
   });
+
   it('should have 5 different separated value', () => {
-    component.increaseDiceSum({name: 'd4', value: 4});
-    component.increaseDiceSum({name: 'd4', value: 4});
-    component.increaseDiceSum({name: 'd4', value: 4});
-    component.increaseDiceSum({name: 'd4', value: 4});
-    component.increaseDiceSum({name: 'd4', value: 4});
-    component.launchDice();
-    expect(component.finalSeparatedValue.length).toBe(17);
+    const service: DiceService = TestBed.get(DiceService);
+
+    service.AddSelectedDice(new Dice('d4', 4));
+    service.AddSelectedDice(new Dice('d4', 4));
+    service.AddSelectedDice(new Dice('d4', 4));
+    const resultRoll:DiceRoll = service.launchDice();
+    expect(resultRoll.separatedValue.length).toBe(17);
   });
 });
